9cfa64b1e5bc6ebbe9fdf55c7e0729be892d15b2
Richard <rich_pryce@yahoo.co.uk>
2020-10-21 15:25:09 +0100

    WIP post validation model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+  p "hello"
+
+  validates :message,
+    length: {
+      minimum: 6, 
+      maximum: 50,
+      too_short: "Post is too short",
+      too_long: "Post is too long"
+    }
+
+
+
 end
