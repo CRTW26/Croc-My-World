514a7f94787ce65bcfe5c1e45da3ca930cfcbe8e
Johnny Yip <jyippy2012@gmail.com>
2020-10-21 16:15:14 +0100

    Still trying to get feature test working for character validation

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,21 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario "Cannot submit posts that less than 3 character" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hi"
+    click_button "Submit"
+    text = page.driver.browser.switch_to.alert.text
+    expect(text).to eq 'Please use at least 3 characters (you are currently using 2 characters) '
+  
+    def check_alert(text)     
+    page.evaluate_script "window.original_alert_function = window.alert"     
+    page.evaluate_script "window.alert = function(msg) { window.lastAlertMsg = msg; }"     
+    yield     last_alert_msg = page.evaluate_script "window.lastAlertMsg"     
+    last_alert_msg.should == text   ensure     
+    page.evaluate_script "window.alert = window.original_alert_function"   
+  end
+  end
 end
