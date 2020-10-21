5daf4cc0a7bac02a9e6c6aa2d688c68a98f4d317
Connor Worthington <worthington.connor@gmail.com>
2020-10-21 10:51:43 +0100

    Adds validation tests for user and model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { described_class.new(password: 'password', email: 'user@user.com') }
+
+  describe 'Validations' do 
+    it 'is valid with valid attributes' do 
+      expect(subject).to be_valid
+    end 
+    it 'is not valid without a password' do 
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end 
+    it 'is not valid without valid email' do 
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  end 
+end
