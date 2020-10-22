21a18938a21b3c992ec768b46aeae660263ac257
Victoria Jusko <vikjusko@gmail.com>
2020-10-22 16:34:34 +0100

    starting work with devise and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :views
   devise_for :users, :controllers => {:registrations => "users"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #match 'users/new/:id', to: 'posts#index'
