db56089155eefd74636f8adfb5cef36e45681ec9
Victoria Jusko <vikjusko@gmail.com>
2020-10-22 17:27:04 +0100

    Devise branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :views
-  devise_for :users, :controllers => {:registrations => "users"}
+  #devise_for :users, :controllers => {:registrations => "users"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #match 'users/new/:id', to: 'posts#index'
   resources :posts
