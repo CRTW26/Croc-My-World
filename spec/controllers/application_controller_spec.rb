5d92b26ebdf5686e391ee133f678f90b64d8d9d3
Johnny Yip <jyippy2012@gmail.com>
2020-10-26 12:18:56 +0000

    Removed commented out test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -8,18 +8,5 @@ RSpec.describe ApplicationController, type: :controller do
             expect(response).to redirect_to new_user_session_path
             expect(response).to have_http_status(302)
         end
-        # it "Logging in should take user to posts index" do
-        #     user = FactoryBot.create(:user)
-        #     sign_in(user)
-        #     get :posts
-        #     expect(response).to have_http_status(200)
-        # end
-
-        # it "Logout should go back to the login" do
-        #     user = FactoryBot.create(:user)
-        #     sign_in(user)
-        #     sign_out(user)
-        #     expect(response).to have_http_status(200)
-        # end
     end
 end
