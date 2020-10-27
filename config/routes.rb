0ae1e088c6862eca2b5343482b0d397816396b97
Connor Worthington <worthington.connor@gmail.com>
2020-10-27 11:58:14 +0000

    Adds friendships controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :comments
+  resources :friendships
 end
---
+++ spec/controllers/friendships_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe FriendshipsController, type: :controller do
+
+  before(:each) do 
+    user = FactoryBot.create(:user)
+    sign_in(user)
+  end 
+
+  describe 'GET/' do 
+    it 'returns page with all friends' do 
+      get :index 
+      expect(response).to have_http_status(200)
+    end 
+  end 
+
+end 
