6f985e423e0c85a28810aaf8dc23d038665b8e19
Richard <rich_pryce@yahoo.co.uk>
2020-10-28 15:29:36 +0000

    User controller and avatar upload working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+  devise_for :users, :path_prefix => 'd' ##
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :likes
   end
-  resources :users
+  resources :users, :only =>[:show] ##
   resources :comments
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
 end
