ae5c94fb6feaa4f328a9f675c5067bf143d18fea
Louis Kirkham <louiskirkham@gmail.com>
2020-10-29 12:41:25 +0000

    added 404 error

--- spec/controllers/users_controllers_spec.rb
+++ spec/controllers/users_controllers_spec.rb
@@ -22,7 +22,12 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET /show' do
     it 'responds with 200' do
       get :show, params: { id: @user.id }
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(200) 
+    end
+
+    it 'responds with 404 if user does not exist' do
+      get :show, params: { id: 27 }
+      expect(response). to have_http_status(404)
     end
   end
 
