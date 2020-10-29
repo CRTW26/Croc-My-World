b95f5b870d0c12bf44e4617d72ec9d2df15f85f0
Connor Worthington <worthington.connor@gmail.com>
2020-10-29 16:29:44 +0000

    removes comments from factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'faker'
 
 FactoryBot.define do
@@ -9,14 +7,10 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password(max_length: 10) }
       trait :with_avatar do
-    # avatar { fixture_file_upload(Rails.root.join('spec', 'assets', 'test-image.jpg'), 'image/jpg') }
         avatar { Rack::Test::UploadedFile.new('spec/assets/test-image.jpg', 'image/jpg') }
       end
       trait :friend do 
         id { 6 } 
-      # name { Faker::Name.name }
-      # email { Faker::Internet.unique.email }
-      # password { Faker::Internet.password(max_length: 10) }
       end 
   end
 end
