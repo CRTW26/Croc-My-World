49f49c4a544cf1f1d34d5130362d07c7bc5a9a77
Connor Worthington <worthington.connor@gmail.com>
2020-10-29 16:22:55 +0000

    removes comments from models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
   validates_presence_of :user_id, :message
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -33,11 +29,6 @@ class User < ApplicationRecord
     friendship.confirmed = true
     friendship.save 
   end
-
-  # def reject(user)
-  #   friendship = inverse_friendships.find { |friendship| friendship.user == user }
-  #   friendship.destroy
-  # end
   
   def friend?(user)
     friends.include?(user)
